db7d789e5db1678a83ae0fd66dfa962979e3e162
Faiz Aziz <ec22497@qmul.ac.uk>
2025-04-22 15:29:26 +0100

    __init__ update

--- src/SPART/__init__.py
+++ src/SPART/__init__.py
@@ -1,4 +1,6 @@
 # __init__.py for SPART package
+
+from .evaluator import Evaluator
 from .optimizer import Optimizer
 from .recommender import Recommender
 from .llm_connector import LLMConnector
@@ -6,6 +8,7 @@ from .local_llm_connector import LocalLLMConnector
 from .external_llm_connector import ExternalLLMConnector
 
 __all__ = [
+    'Evaluator', 
     'Optimizer', 
     'Recommender', 
     'LLMConnector', 
