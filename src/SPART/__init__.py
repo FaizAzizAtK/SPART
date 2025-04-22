acb3626ac4e8ce01df9270faf9cf99ee0016886a
Faiz Aziz <ec22497@qmul.ac.uk>
2025-04-22 15:30:33 +0100

    fix: update package initialization imports

--- src/SPART/__init__.py
+++ src/SPART/__init__.py
@@ -1,16 +1,16 @@
 # __init__.py for SPART package
 
-from .evaluator import Evaluator
-from .optimizer import Optimizer
-from .recommender import Recommender
+from .evaluator import PromptEvaluator
+from .optimizer import PromptOptimizer
+from .recommender import PromptRecommender
 from .llm_connector import LLMConnector
 from .local_llm_connector import LocalLLMConnector
 from .external_llm_connector import ExternalLLMConnector
 
 __all__ = [
-    'Evaluator', 
-    'Optimizer', 
-    'Recommender', 
+    'PromptEvaluator', 
+    'PromptOptimizer', 
+    'PromptRecommender', 
     'LLMConnector', 
     'LocalLLMConnector', 
     'ExternalLLMConnector'
